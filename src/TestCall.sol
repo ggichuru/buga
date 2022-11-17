67a2b542d8544c7d753d16b92f7d0f8b50ca270b
ggichuru <ggichuru.dev@gmail.com>
2022-11-18 02:11:04 +0300

    Testing Call functionality when calling a function in another contract

---
+++ src/TestCall.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract TestCall {
+    string public message;
+    uint256 public x;
+
+    event Log(string message);
+
+    fallback() external payable {
+        emit Log("fallback was called");
+    }
+
+    function foo(string memory _message, uint256 _x)
+        external
+        payable
+        returns (bool, uint256)
+    {
+        message = _message;
+        x = _x;
+        return (true, 999);
+    }
+}
+
+contract Call {
+    bytes public data;
+
+    function callFoo(address _testCallAddr) external payable {
+        (bool success, bytes memory _data) = _testCallAddr.call{value: 1000}(
+            abi.encodeWithSignature("foo(string,uint256)", "calling foo", 123)
+        );
+
+        require(success, "Call failed");
+        data = _data;
+    }
+
+    // calling a function that doesnt exist will invoke the fallback
+    // Without fallback ,the tx would fail 
+    function callDoesntExist(address _test) external {
+        (bool success, ) = _test.call(abi.encodeWithSignature("callDoesnt()"));
+
+        require(success, "call failed");
+    }
+}
