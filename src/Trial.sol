1352fe7c892fc2cc03fe2a609a144877a0c9c8a1
ggichuru <ggichuru.dev@gmail.com>
2022-12-19 14:56:22 +0300

    enums and receive fn

--- src/Trial.sol
+++ src/Trial.sol
@@ -2,7 +2,35 @@
 pragma solidity ^0.8.13;
 
 contract Trial {
-    function addOne(uint256 x) external pure returns (uint256) {
-        return ++x;
+    function _getContractBalance() external view returns (uint256 _balance) {
+        _balance = address(this).balance;
+    }
+
+    receive() external payable {}
+}
+
+contract EnumTest {
+    // Create an enum
+    enum ActionChoices {
+        GoUp,
+        GoDown,
+        StayStill,
+        TurnAround
+    }
+
+    // Statevariables to access enum
+    ActionChoices choice;
+    ActionChoices constant DEFAULT_CHOICE = ActionChoices.StayStill;
+
+    function setGoUp() public {
+        choice = ActionChoices.GoUp;
+    }
+
+    function getChoice() public view returns (ActionChoices) {
+        return choice;
+    }
+
+    function getDefaultChoice() public pure returns (uint256) {
+        return uint256(DEFAULT_CHOICE);
     }
 }
