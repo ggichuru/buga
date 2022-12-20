c3e53816cbc579960a126f9859b77c563e29a914
ggichuru <ggichuru.dev@gmail.com>
2022-12-20 13:44:59 +0300

    Datalocations

--- src/Trial.sol
+++ src/Trial.sol
@@ -52,3 +52,34 @@ contract Pyramid {
         return x + y;
     }
 }
+
+contract DataLocations {
+    /// @notice b2n storage and memory (or from calldata) always create an independent copy.
+    /// @notice from memory to memory -> only create references.
+    //         changes to 1 memory variable are visible in all ohter mem vars refering to the same data
+    /// @notice from storage to a local storage variable -> only assign a reference.
+
+    /// @notice the data location of x is storage.
+    // This is the only place where the data location can be ommited
+    uint256[] x;
+
+    /// @notice the data location memoryArray is memory
+    function f(uint256[] memory memoryArray) public {
+        x = memoryArray; /// @dev copies the whole array to storage
+
+        uint256[] storage y = x; /// @dev assigns a pointer // data location of y is storage
+        y[7]; /// @return 8th element;
+        y.pop(); /// @dev remove the last element in x
+        delete x; /// @dev clears the array and also modifies y
+
+        /// @dev `delete y` is not valid as assignmnets to local variables ref storage can only be made from exisiting storage objects.
+        // it would resset the pointer but there's no sensible location it could point to.
+
+        g(x); /// @dev calls g handing over a reference to x
+        h(x); /// @dev calls h and creates an independent, temporary copy in memory
+    }
+
+    function g(uint256[] storage) internal pure {}
+
+    function h(uint256[] memory) public pure {}
+}
